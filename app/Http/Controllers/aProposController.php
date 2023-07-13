e1b34b2d0e9f2ab9a48c72bad32e47915b4c90da
Jose <jode2308@hotmail.com>
2023-07-13 08:38:29 +0200

    controller

--- app/Http/Controllers/aProposController.php
+++ app/Http/Controllers/aProposController.php
@@ -6,20 +6,25 @@ use Illuminate\Http\Request;
 
 class aProposController extends Controller
 {
+    private array $members = [
+            ['names' => 'Matthieu'],
+            ['names' => "Fiorella"],
+            ['names' => "Toto"],            
+        ];
+
     public function person()
     {
         return view('a-propos', [
         'sujet' => 'Laravel',
-        'members' => [
-            ['names' => 'Matthieu'],
-            ['names' => "Fiorella"],
-            ['names' => "Toto"],            
-            ]
+        'members' => $this->members
         ]);
     }
 
     public function aproposPerson(string $user)
     {
+        if (!in_array($user, array_column($this->members, 'names'))){
+            abort(404);
+        }
         return view('about-show', ['user' => $user]); 
     }
 }
